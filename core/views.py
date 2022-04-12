0d29bcc7ffec4b084a3d2248cc789eeaeb7e61a5
david <maldonadopereira@gmail.com>
2022-04-12 08:22:35 -0300

    -Testar deploy

--- core/views.py
+++ core/views.py
@@ -5,7 +5,8 @@ def index(request):
     return render(request, 'index.html')
 
 def importa_transacao(request):
-    if request.method == 'POST':
+    pass
+   ''' if request.method == 'POST':
         if 'transacao' in request.FILES:
             transacao = request.FILES['transacao']
             print(f'Nome no Arquivo: {transacao.name}\nTamanho: {(transacao.size)/1000000} MegaBytes')
@@ -17,4 +18,4 @@ def importa_transacao(request):
             return redirect('index')
         else:
             print('Você não importou nenhum arquivo!!')
-            return redirect('index')
+            return redirect('index')'''
