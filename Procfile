7a8a8105217b3f7a8f2f4af552bc3dc5e529a87a
david <maldonadopereira@gmail.com>
2022-04-12 08:09:50 -0300

    -Instalação do gunicorn

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn alura.wsgi--log-file -
--- alura/settings.py
+++ alura/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # 3rd party
     'crispy_forms',
+    'gunicorn',
     # Apps Locais
     'core',
 ]
