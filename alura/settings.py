807f5340b79dc85bfffc31b6df56b1f40f4a1d76
david <maldonadopereira@gmail.com>
2022-04-14 09:01:19 -0300

    -Implementada Leitura de aquivos CSV

--- alura/settings.py
+++ alura/settings.py
@@ -126,7 +126,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Media Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
